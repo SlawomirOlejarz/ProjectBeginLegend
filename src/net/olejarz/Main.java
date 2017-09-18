52d34845c8e3df7d42859913c6c4dfb700859689
SlawomirOlejarz <slawomir.olejarz@gmail.com>
2017-09-18 23:37:27 +0200

    first commit

---
+++ src/net/olejarz/Main.java
@@ -0,0 +1,22 @@
+package net.olejarz;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Super Manager");
+
+        Team team1 = new Team();
+        team1.setName("FKS STAL MIELEC");
+        team1.setTeampower(100);
+
+        Team team2 = new Team();
+        team2.setName("LEGIA WARSZAWA");
+        team2.setTeampower(101);
+
+        if(team1.getTeampower() > team2.getTeampower()){
+            System.out.println("Wygrywa " + team1.getName() + " 5:1");
+        } else {
+            System.out.println("Wygrywa " + team2.getName() + " 5:1");
+        }
+    }
+}
---
+++ src/net/olejarz/Team.java
@@ -0,0 +1,22 @@
+package net.olejarz;
+
+public class Team {
+    private String name;
+    private int teampower;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTeampower() {
+        return teampower;
+    }
+
+    public void setTeampower(int teampower) {
+        this.teampower = teampower;
+    }
+}
